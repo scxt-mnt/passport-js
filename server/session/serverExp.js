37c7c7246c4cf6d24f182f90ab998e73280aa079
Scott <scottboragay06@gmail.com>
2025-10-19 20:14:02 +0800

    sessions & MongoStore

--- server/session/serverExp.js
+++ server/session/serverExp.js
@@ -3,22 +3,49 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import user from '../../models/user.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.APP_PORT;
 const DB_URL = process.env.APP_URL;
+const SECRET = process.env.SECRET;
 
-//middleware
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 mongoose.connect(DB_URL)
-    .then(() => app.listen(PORT, () => console.log("listening to port " + PORT)))
+    .then(() => {
+        app.listen(PORT, () => console.log("listening to port " + PORT))
+
+
+        const sessionStore = MongoStore.create({
+            mongoUrl: DB_URL,
+            collectionName: "sessions"
+        });
+
+        //middlewares
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(cors());
+
+        app.use(session({
+            secret: SECRET,
+            resave: false,
+            saveUninitialized: true,
+            store: sessionStore,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24
+            }
+        }))
+
+
+    })
     .catch((err) => { console.log(err) });
+
+
+
+
 
 
 

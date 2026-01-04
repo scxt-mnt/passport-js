d8d9d355b4a24f3193e8387e304dfc7f01c38044
Scott <scottboragay06@gmail.com>
2026-01-04 22:05:19 +0800

    added: cookie session with cookie-session middleware

--- server/session/serverExp.js
+++ server/session/serverExp.js
@@ -1,40 +1,32 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
 import router from '../../routes/auth-routes.js'
 import passport from 'passport';
+import cookieSession from 'cookie-session';
 import '../../config/passport-setup.js';
+
 dotenv.config();
 
 const app = express();
 
+
 const PORT = process.env.APP_PORT;
 const DB_URL = process.env.APP_URL;
 const SECRET = process.env.SECRET;
+const sessionKey = process.env.sessionKey;
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.set("view engine", "ejs");
+app.use(cookieSession({
+    maxAge:  1000 * 60  * 60  *  24,
+    keys: [sessionKey]
+}));
 
-const sessionStore = MongoStore.create({
-    mongoUrl: DB_URL,
-    collectionName: "sessions"
-});
-
-app.use(session({
-    secret: SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24
-    }
-}))
 
 app.use(passport.initialize());
 app.use(passport.session());
